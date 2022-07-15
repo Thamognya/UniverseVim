367791e247fc666dc810f38f0337ce98fd58814d
Thamognya Kodi <contact@thamognya.com>
2022-07-15 10:57:53 +0700

    update

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
-pcall(require, 'impatient')
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then impatient.enable_profile() end
 
-require('packer_commands')
-require('globals')
-require('config')
-require('settings')
-require('colorscheme')
-require('keymappings')
-require('autocmds')
-require('functions')
+for _, source in ipairs {
+  "core.utils",
+  "core.options",
+  "core.bootstrap",
+  "core.plugins",
+  "core.autocmds",
+  "core.mappings",
+  "core.ui",
+  "configs.which-key-register",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
 
-require('lsp.config')
-require('lsp.setup')
-require('lsp.functions')
-
-require('snippets.react')
+astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
